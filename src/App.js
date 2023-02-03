7772dc747261868ee39bd6582b00529dd00d980b
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-02-03 15:48:53 -0500

    added main imports to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+// Main Imports
+import { Calendar, dateFnsLocalizer} from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import React, {useState} from "react";
+import DatePicker from "react-datepicker";
+
+
+
+
+// Style Imports
 import './App.css';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
 
 function App() {
   return (
