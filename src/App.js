3a37ea729468a7974e17205b12245a3faa8863d9
KaptSprinkles <KSoto18@users.noreply.github.com>
2023-02-03 16:07:59 -0500

    added calendar to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 // Main Imports
-import { Calendar, dateFnsLocalizer} from "react-big-calendar";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import format from "date-fns/format";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-
-
-
 
 // Style Imports
 import './App.css';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 
+const locales = {
+  "en-US": require("date-fns/locale/en-US")
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+})
+
+const bills = [
+  {
+    title: "Capital One",
+    start: new Date(2023, 3, 0),
+    end: new Date(2023, 3, 0)
+  },
+
+  {
+    title: "Netflix",
+    start: new Date(2023, 4, 0),
+    end: new Date(2023, 4, 0)
+  },
+
+]
+
 function App() {
   return (
+
     <div className="App">
-     
+
+      <Calendar 
+      localizer={localizer} 
+      bills={bills} 
+      startAccessor="start" 
+      endAccessort="end" 
+      style={{height: 500, margin: "100px"}}
+      />
+
     </div>
   );
 }
